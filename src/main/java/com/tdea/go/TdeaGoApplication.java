aa7fb840e5ba924f1f0682a3347325ce81f1808f
Monica <liliatehito@gmail.com>
2026-05-06 21:11:34 -0500

    creación de TdeaGoApplication

---
+++ src/main/java/com/tdea/go/TdeaGoApplication.java
@@ -0,0 +1,13 @@
+package com.tdea.go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdeaGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdeaGoApplication.class, args);
+    }
+
+}
